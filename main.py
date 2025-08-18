86dcbc3a179ab5e77752a39f6e96e96ce00ce67a
Muhammad Ardhan Aulia Rahman <ardhanaulia@gmail.com>
2025-08-18 10:21:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -227,5 +227,5 @@ def proxy_video():
         return f"Gagal memuat video melalui proxy: {e}", 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
