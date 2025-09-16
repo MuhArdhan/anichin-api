44e3427526f7364e644d37ba6021f0fafe1606c7
Muhammad Ardhan Aulia Rahman <ardhanaulia@gmail.com>
2025-09-16 18:59:50 +0700

    Update parsing.py

--- api/utils/parsing.py
+++ api/utils/parsing.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from os import getenv
 from requests import Session
+import dns.resolver
+import socket
 
 load_dotenv()
 
@@ -11,17 +13,35 @@ class Parsing(Session):
         super().__init__()
         self.url = getenv("HOST", "")
         self.history_url = None
+        self.user_agent = getenv("USER_AGENT", "")
+        self.dns = getenv("DNS", "8.8.8.8")  # default Google DNS
+
+        # Set default headers
+        if self.user_agent:
+            self.headers.update({"User-Agent": self.user_agent})
+
+    def resolve_with_custom_dns(self, hostname):
+        resolver = dns.resolver.Resolver()
+        resolver.nameservers = [self.dns]
+        answer = resolver.resolve(hostname, "A")
+        return answer[0].to_text()
 
     def __get_html(self, slug, **kwargs):
         if slug.startswith("/"):
             url = f"{self.url}{slug}"
         else:
             url = f"{self.url}/{slug}"
-        print(kwargs)
-        r = self.get(url, **kwargs)
+
+        # Resolve host pakai DNS custom
+        hostname = self.url.replace("https://", "").replace("http://", "")
+        ip = self.resolve_with_custom_dns(hostname)
+        print(f"[INFO] Resolving {hostname} via {self.dns} => {ip}")
+
+        # Ganti host ke IP, tapi tetap kasih Host header agar HTTPS tidak error
+        r = self.get(url, headers={"Host": hostname, **self.headers}, **kwargs)
+
         self.history_url = url
-        resp = r.text
-        return resp
+        return r.text
 
     def get_parsed_html(self, url, **kwargs):
         return BeautifulSoup(self.__get_html(url, **kwargs), "html.parser")
